ffca7471c9b95e30c81b9b0498a6f92ee445dec7
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-08-19 15:14:20 -0600

    boiler plate knex and express is set up

---
+++ index.js
@@ -0,0 +1,5 @@
+const server = require('./server')
+
+const port = process.env.port || 8000
+
+server.listen(port, () => console.log(`Server is listenting on port ${port}`))
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const server = express()
+
+// const AuthRouter = require('./auth-route')
+
+server.use(express.json())
+server.use('/api', AuthRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json({ message: 'all good'})
+})
+
+module.exports = server
