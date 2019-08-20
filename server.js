74d6508db7127e1a9d3e89ce1951f92a42024ba4
BrandonHarrison01 <49922064+BrandonHarrison01@users.noreply.github.com>
2019-08-20 16:19:39 -0600

    dependency added and session options middleware built

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const session = require('express-session')
 
 const server = express()
 
 const AuthRouter = require('./auth/auth-route')
 
+const sessionOptions = {
+    name: 'sessionname',
+    secret: process.env.COOKIE_SECRET || 'this is a secret',
+    cookie: {
+        secure: process.env.COOKIE_SECURE || false,
+        maxAge: 1000 * 60 * 60 * 24,
+        httpOnly: true,
+    },
+    resave: false,
+    saveUninitialized: true
+}
+
 server.use(express.json())
+server.use(session(sessionOptions))
 server.use('/api', AuthRouter)
 
 server.get('/', (req, res) => {
